81b71aaab1284b4e368ce8e1449cb4bfd960f3e5
Fatih KÜYÜK <fatih.kuyuk@hotmail.com>
2025-01-03 20:28:08 +0300

    Update first_ins_almalinux.sh

--- first_ins_almalinux.sh
+++ first_ins_almalinux.sh
@@ -1,6 +1,7 @@
 timedatectl set-timezone Europe/Istanbul
 yum update -y
 yum install epel-release -y
+yum install fail2ban -y
 systemctl enable fail2ban
 systemctl start fail2ban
 systemctl status fail2ban
