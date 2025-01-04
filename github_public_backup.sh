289e780d99fb0a27ef6aaeee4522e5bed386df94
Fatih KÜYÜK <fatih.kuyuk@hotmail.com>
2025-01-04 18:06:20 +0300

    Create github_public_backup.sh

---
+++ github_public_backup.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+wget -O "hetzner.zip_$(date +%d_%m_%y)" https://github.com/fthrsya/hetzner-proxmox-single/archive/refs/heads/main.zip
+
+wget -O "bash.zip_$(date +%d_%m_%y)" https://github.com/fthrsya/bash/archive/refs/heads/main.zip
+
+wget -O "python.zip_$(date +%d_%m_%y)" https://github.com/fthrsya/Python/archive/refs/heads/master.zip
+
+wget -O "python_virustotal.zip_$(date +%d_%m_%y)" https://github.com/fthrsya/python_virustotal_scan/archive/refs/heads/main.zip
+
+wget -O "oracle.zip_$(date +%d_%m_%y)" https://github.com/fthrsya/oracle_scriptler/archive/refs/heads/main.zip
+
+wget -O "oracle_script_tanelpoder.zip_$(date +%d_%m_%y)" https://github.com/fthrsya/oracle_scripts/archive/refs/heads/master.zip
